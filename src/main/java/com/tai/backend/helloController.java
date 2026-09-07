b872de8bcb9b7437bf2c84c9a895db3d4312d943
Shaimal <shaimalabdulla35@gmail.com>
2026-09-07 09:08:25 +0500

    Fix routing bug

--- src/main/java/com/tai/backend/helloController.java
+++ src/main/java/com/tai/backend/helloController.java
@@ -2,12 +2,20 @@ package com.tai.backend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
+@RequestMapping("/home")
 public class helloController {
 
     @GetMapping("/hello")
     public String hello() {
         return "Test ping";
+    }
+
+    @GetMapping("/hello/{name}")
+    public String hello(@PathVariable String name) {
+        return String.format("Hello %s", name);
     }
 }
